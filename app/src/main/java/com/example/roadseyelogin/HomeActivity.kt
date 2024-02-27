857e05f741258126ff05299feca5cde824ceb382
Gaurav <gaurav1999.5@gmail.com>
2024-02-28 01:29:49 +0530

    completed backend for HomeActivity

--- app/src/main/java/com/example/roadseyelogin/HomeActivity.kt
+++ app/src/main/java/com/example/roadseyelogin/HomeActivity.kt
@@ -1,19 +1,30 @@
 package com.example.roadseyelogin
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.roadseyelogin.databinding.ActivityHomeBinding
 import com.example.roadseyelogin.databinding.ActivityOtpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var auth: FirebaseAuth
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val mobileNo = String.format("+91-%s", intent.getStringExtra("mobile"))
-        binding.greet.text = mobileNo
+        binding.greet.text = "Hello\n$mobileNo"
+        auth = FirebaseAuth.getInstance()
+
+        binding.signOut.setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
