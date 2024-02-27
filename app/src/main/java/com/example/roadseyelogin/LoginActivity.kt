2da14148aa15fbe127d84fa9914789f0e4a2d1c0
Gaurav <gaurav1999.5@gmail.com>
2024-02-27 18:04:09 +0530

    implemented backend for both the activities

--- app/src/main/java/com/example/roadseyelogin/LoginActivity.kt
+++ app/src/main/java/com/example/roadseyelogin/LoginActivity.kt
@@ -1,13 +1,38 @@
 package com.example.roadseyelogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.EditText
+import android.widget.Toast
+import com.example.roadseyelogin.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.getOtp.setOnClickListener{ view ->
+                val number =  binding.inputMobileNumber.text?.toString()?.trim() ?: ""
+                if (number.isNotEmpty() && number.length == 10) {
+                    val intent = Intent(view.context, OtpActivity::class.java)
+                    intent.putExtra("mobile", number)
+                    startActivity(intent)
+                } else {
+                    val message = if (number.isEmpty()) {
+                        "Enter Mobile number"
+                    } else {
+                        "Please enter correct number"
+                    }
+                    Toast.makeText(view.context, message, Toast.LENGTH_SHORT).show()
+                }
+
+
+        }
     }
 }
--- app/src/main/java/com/example/roadseyelogin/OtpActivity.kt
+++ app/src/main/java/com/example/roadseyelogin/OtpActivity.kt
@@ -2,10 +2,105 @@ package com.example.roadseyelogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import com.example.roadseyelogin.databinding.ActivityOtpBinding
+
 
 class OtpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOtpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_otp)
+        binding = ActivityOtpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mobileNo.text = String.format("+91-%s", intent.getStringExtra("mobile"))
+
+        binding.getOtp.setOnClickListener {view ->
+            val n1 = binding.inputotp1.text.toString().trim()
+            val n2 = binding.inputotp1.text.toString().trim()
+            val n3 = binding.inputotp1.text.toString().trim()
+            val n4 = binding.inputotp1.text.toString().trim()
+            val n5 = binding.inputotp1.text.toString().trim()
+            val n6 = binding.inputotp1.text.toString().trim()
+
+            val message = if (n1.isNotEmpty() && n2.isNotEmpty() && n3.isNotEmpty()
+                && n4.isNotEmpty() && n5.isNotEmpty() && n6.isNotEmpty()) {
+                // Your logic for verifying the numbers goes here
+                "OTP verified"
+            } else {
+                "Please enter all numbers"
+            }
+            Toast.makeText(view.context, message, Toast.LENGTH_SHORT).show()
+        }
+
+        numberOtpMove()
     }
+
+    private fun numberOtpMove() {
+
+        binding.inputotp1.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.toString().trim().isEmpty()) {
+                    binding.inputotp2.requestFocus()
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        binding.inputotp2.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.toString().trim().isEmpty()) {
+                    binding.inputotp3.requestFocus()
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        binding.inputotp3.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.toString().trim().isEmpty()) {
+                    binding.inputotp4.requestFocus()
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        binding.inputotp4.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.toString().trim().isEmpty()) {
+                    binding.inputotp5.requestFocus()
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        binding.inputotp5.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.toString().trim().isEmpty()) {
+                    binding.inputotp6.requestFocus()
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+    }
+
 }
